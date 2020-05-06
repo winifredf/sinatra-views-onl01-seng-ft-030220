2514378a22585e22053e66f511206ae718771a75
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-06 11:27:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+# 	get '/' do
+# 		"<h1>Hello World</h1>"
+# 	end
+	
 	get '/' do
-		
+	  erb :index
 	end
+	
+	get '/' do
+	  "Testing the info page"
+	end
+	
+	get "/info" do
+      erb :info
+  end
 end
